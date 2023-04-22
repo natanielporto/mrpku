6971ba24df66e47d1233de2cbf7a01df784ada8d
Renato Hideki Osaka <renato.osaka@hotmail.com>
2023-04-22 12:13:21 -0300

    feat: tab navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button } from "./src/Button/Button";
-import { SafeAreaView } from "react-native";
+import { Routes } from "./src/routes";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Button />
-    </SafeAreaView>
+    <>
+      <Routes />
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+    </>
   );
 }
