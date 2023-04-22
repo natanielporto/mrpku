077e50ce4831285a788a1e1b09d75c3da3c57266
Nataniel Porto <natanielkporto@gmail.com>
2023-04-22 21:15:52 +0100

    add colors and fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Button } from "./src/Button/Button";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native";
+import Btn from "./src/components/atoms/Button/Button";
+import * as Font from "expo-font";
+import { FontSource } from "expo-font";
+
+type FontMap = Record<string, FontSource> & {
+  [fontName: string]: FontSource;
+};
 
 export default function App() {
+  const handlePress = () => 1;
+
+  async function loadFonts(fonts: FontMap) {
+    await Font.loadAsync(fonts);
+  }
+
+  useEffect(() => {
+    loadFonts({
+      "Hind-Bold": require("./assets/fonts/Hind-Bold.ttf"),
+      "Hind-Light": require("./assets/fonts/Hind-Light.ttf"),
+      "Hind-Medium": require("./assets/fonts/Hind-Medium.ttf"),
+      "Hind-Regular": require("./assets/fonts/Hind-Regular.ttf"),
+      "Hind-SemiBold": require("./assets/fonts/Hind-SemiBold.ttf"),
+    });
+  }, []);
+
   return (
     <SafeAreaView>
-      <Button />
+      <Btn title="Test Case" onPress={handlePress} />
     </SafeAreaView>
   );
 }
