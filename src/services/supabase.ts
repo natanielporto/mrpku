8f777a9b60c86e164267d4741dbe02b8cec1b770
Renato Hideki Osaka <renato.osaka@hotmail.com>
2023-04-22 14:45:50 -0300

    feat: url polyfill, app config

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,8 @@
+import "react-native-url-polyfill/auto";
+import Constants from "expo-constants";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPASEANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_URL = Constants.expoConfig?.extra?.SUPABASE_URL;
+const SUPASEANON_KEY = Constants.expoConfig?.extra?.SUPABASEANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPASEANON_KEY);
