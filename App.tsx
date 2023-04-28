c73021777b2ff041898fec3badc6c02cb9eda9cd
Renato Hideki Osaka <renato.osaka@hotmail.com>
2023-04-28 20:35:34 -0300

    feat: sync provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Routes } from "./src/routes";
 import { View } from "react-native";
-import { Database } from "./src/services/database";
-import { Sync } from "./src/services/sync";
+import { AppProvider } from "./src/context";
 import Constants from "expo-constants";
 import * as Sentry from "sentry-expo";
 import "react-native-gesture-handler";
 import "react-native-get-random-values";
 
-console.log("DEV", __DEV__);
-console.log("Sentry DSN", Constants.expoConfig?.extra?.SENTRY_DSN);
 Sentry.init({
   dsn: Constants.expoConfig?.extra?.SENTRY_DSN,
   enableInExpoDevelopment: true,
@@ -20,23 +17,12 @@ Sentry.init({
 });
 
 export default function App() {
-  Database.instance.sql("SELECT * FROM config").then((data) => {
-    console.log("Database Data", data);
-  });
-
-  useEffect(() => {
-    const sync = new Sync();
-    sync.execute();
-  }, []);
-
-  Database.instance.sql("SELECT * FROM recipe").then((data) => {
-    console.log(data);
-  });
-
   return (
-    <View style={{ flex: 1, backgroundColor: "#fafafa" }}>
-      <Routes />
-      <StatusBar style="dark" translucent backgroundColor="transparent" />
-    </View>
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: "#fafafa" }}>
+        <Routes />
+        <StatusBar style="dark" translucent backgroundColor="transparent" />
+      </View>
+    </AppProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,25 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
+import { useSync } from "../hooks/use-sync";
+import { ActivityIndicator, View } from "react-native";
 
 export function Routes() {
+  const { isSyncing } = useSync();
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <AppRoutes />
-      </NavigationContainer>
+      {isSyncing ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" color="#000" />
+        </View>
+      ) : (
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
+      )}
     </SafeAreaView>
   );
 }
