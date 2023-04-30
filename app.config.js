0bf18bf89892e9413af3b47e08f2e878f9c47c75
Renato Hideki Osaka <renato.osaka@hotmail.com>
2023-04-30 12:12:57 -0300

    fix: supabase service role key

--- app.config.js
+++ app.config.js
@@ -32,6 +32,7 @@ export default {
       SENTRY_DSN: process.env.SENTRY_DSN,
       SUPABASE_URL: process.env.SUPABASE_URL,
       SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+      SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
       eas: {
         projectId: "d4a14fc6-ac46-4f33-b182-9ad819913afd",
       },
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -4,6 +4,10 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "../@types/supabase";
 
 const SUPABASE_URL = Constants.expoConfig?.extra?.SUPABASE_URL;
-const SUPABASE_ANON_KEY = Constants.expoConfig?.extra?.SUPABASE_ANON_KEY;
+const SUPABASE_SERVICE_ROLE_KEY =
+  Constants.expoConfig?.extra?.SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(
+  SUPABASE_URL,
+  SUPABASE_SERVICE_ROLE_KEY
+);
